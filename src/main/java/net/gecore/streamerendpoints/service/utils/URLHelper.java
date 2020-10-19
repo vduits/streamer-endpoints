59eb1298e5b6ba35ba8afe0f552655287a9e702f
PS2emu <17675927+PS2emu@users.noreply.github.com>
2020-10-18 21:55:48 -0400

    Created buildIGDBUrl, modified buildTwitchUrl, moved to Utils

--- src/main/java/net/gecore/streamerendpoints/service/utils/URLHelper.java
+++ src/main/java/net/gecore/streamerendpoints/service/utils/URLHelper.java
@@ -1,8 +1,10 @@
-package net.gecore.streamerendpoints.service.twitch.component;
+package net.gecore.streamerendpoints.service.utils;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import net.gecore.streamerendpoints.configuration.TwitchConfig;
+import net.gecore.streamerendpoints.service.igdb.IGDBAPIException;
+import net.gecore.streamerendpoints.service.igdb.constants.IGDBEndpoint;
 import net.gecore.streamerendpoints.service.twitch.TwitchAPIException;
 import net.gecore.streamerendpoints.service.twitch.constants.TwitchEndpoint;
 import org.slf4j.Logger;
@@ -13,8 +15,8 @@ public class URLHelper {
   public final static Logger LOGGER = LoggerFactory.getLogger(URLHelper.class);
 
 
-  public static URL buildUrl(TwitchConfig twitchConfig, TwitchEndpoint twitchEndpoint,
-      String urlParams) throws TwitchAPIException {
+  public static URL buildTwitchUrl(TwitchConfig twitchConfig, TwitchEndpoint twitchEndpoint,
+                                   String urlParams) throws TwitchAPIException {
     try {
       return new URL(
           "https://api.twitch.tv/"
@@ -25,6 +27,18 @@ public class URLHelper {
     } catch (MalformedURLException me) {
       LOGGER.error("Encountered an issue trying to build the url: " + me.getMessage());
       throw new TwitchAPIException("There is configuration issue contacting the twitch api");
+    }
+  }
+
+  public static URL buildIGDBUrl(IGDBEndpoint endpoint) throws IGDBAPIException {
+    try{
+      return new URL(
+              "https://api.igdb.com/v4/"
+              + endpoint
+      );
+    } catch (MalformedURLException me) {
+      LOGGER.error("Encountered an issue trying to build the url: " + me.getMessage());
+      throw new IGDBAPIException("There is a configuration issue contacting the IGDB api");
     }
   }
 
