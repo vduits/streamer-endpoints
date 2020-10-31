aad80a51bb40bc470dad8162a3894c3855c4f616
PS2emu <17675927+PS2emu@users.noreply.github.com>
2020-10-31 18:47:05 -0400

    retrieveGameInfo tying igdbGameService and StreamService

--- src/main/java/net/gecore/streamerendpoints/controller/IGDBController.java
+++ src/main/java/net/gecore/streamerendpoints/controller/IGDBController.java
@@ -2,23 +2,39 @@ package net.gecore.streamerendpoints.controller;
 
 import java.util.List;
 
+import net.gecore.streamerendpoints.domain.TwitchGame;
+import net.gecore.streamerendpoints.service.igdb.IGDBAPIException;
 import net.gecore.streamerendpoints.service.igdb.IGDBGameService;
-import org.springframework.web.bind.annotation.*;
+import net.gecore.streamerendpoints.service.twitch.StreamService;
+import net.gecore.streamerendpoints.service.twitch.TwitchAPIException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("${rest.api}" + "/user/{userId}/stream/game")
 public class IGDBController {
 
     private final IGDBGameService igdbGameService;
+    private final StreamService streamService;
 
-    public IGDBController(IGDBGameService igdbGameService){
+    @Autowired
+    public IGDBController(IGDBGameService igdbGameService, StreamService streamService){
         this.igdbGameService = igdbGameService;
+        this.streamService = streamService;
     }
 
     @GetMapping(value = "/{userId}/stream/game")
-    public void retrieveGameInfo(@PathVariable Integer userId,
-                                 @RequestParam(required = false) List<String> filters){
+    public String retrieveGameInfo(@PathVariable Integer userId,
+        @RequestParam(value = "summary,url", required = false) List<String> filters)
+            throws TwitchAPIException, IGDBAPIException {
 
+        TwitchGame twitchGame = streamService.retrieveCurrentPlayingGameByUserId(userId);
+        Integer exReply = igdbGameService.retrieveIdByExternalGame(twitchGame.getId());
+        return igdbGameService.retrieveGameById(exReply);
     }
 
 }
